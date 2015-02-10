93acf470cdd0ea6fa81406dcdf7b3e670f017a65
黄记新 <635152644@qq.com>
2015-02-10 15:15:01 +0800

    数据库定时备份

---
+++ JCinCMS/src/main/java/com/jcin/cms/service/impl/DataBaseBackupTaskImpl.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.jcin.cms.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.jcin.cms.domain.Mysqlbackup;
+import com.jcin.cms.service.IDataBaseBackupTask;
+import com.jcin.cms.service.IMySQLService;
+
+/**
+ * @author Administrator
+ *
+ */
+@Service
+public class DataBaseBackupTaskImpl implements IDataBaseBackupTask {
+
+	@Resource
+	private IMySQLService mySQLService;
+	
+	/* (non-Javadoc)
+	 * @see com.jcin.cms.service.IDataBaseBackupTask#backup()
+	 */
+	@Override
+	@Scheduled(cron="0 0 23 * * ?")   //每天晚上十一點点触发 
+	public void backup() {
+//		Mysqlbackup record = new  Mysqlbackup();
+//		mySQLService.insert(record);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
